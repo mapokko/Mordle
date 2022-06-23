c05a176c8e0a0278b62d46f2fbd1e03c61c76312
mapokko <mapokko@hotmail.it>
2022-06-23 16:11:33 +0200

    initial commit

---
+++ App2.js
@@ -0,0 +1,37 @@
+import { View, StyleSheet } from 'react-native'
+import React from 'react'
+
+import { Text } from "@rneui/themed";
+import { Input } from "@rneui/themed";
+import { Button } from "@rneui/base";
+
+const App2 = () => {
+  return (
+    <View style={s.view}>
+        <Text h2 style={{marginBottom: 55, marginTop: 40}}>Iscriviti per giocare!</Text>
+        <Text h4>Mail</Text>
+        <Input containerStyle={{width: '80%', marginTop: 20}} placeholder='Inserisci la mail'/>
+
+        <Text h4 style={{marginTop: 30}}>Password</Text>
+        <Input containerStyle={{width: '80%', marginTop: 20}} placeholder='Inserisci la password' secureTextEntry={true}/>
+
+        <Button containerStyle={{marginTop: 15}} titleStyle={{fontSize: 20}} size='md' color='success' title='REGISTRATI'/>
+
+        <Text style={{fontSize: 15, marginTop: 25, color: '#000', fontWeight: 'bold'}}>Gia' iscritto? fai Log-in!</Text>
+    </View>
+  )
+}
+
+const s = StyleSheet.create({
+    view: {
+        display: 'flex',
+        height: '100%',
+        justifyContent: 'flex-start',
+        alignItems: 'center'
+    },
+    input: {
+        width: '80%'
+    }
+})
+
+export default App2
